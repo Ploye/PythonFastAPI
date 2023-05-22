d494b667376f4da8a4e9b2f1df1a599c07b9e203
Ploye <musa7076@gmail.com>
2023-05-22 14:14:40 +0700

    first commit

---
+++ alembic/versions/67b53b3ecdc8_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 67b53b3ecdc8
+Revises: c9cd035627c7
+Create Date: 2023-05-22 13:09:47.014053
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '67b53b3ecdc8'
+down_revision = 'c9cd035627c7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ alembic/versions/c9cd035627c7_create_users_table.py
@@ -0,0 +1,24 @@
+"""create users table
+
+Revision ID: c9cd035627c7
+Revises: 
+Create Date: 2023-05-22 13:05:46.507129
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c9cd035627c7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
