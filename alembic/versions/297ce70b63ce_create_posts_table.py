5d7b47f2d24231004ed71a451cbda0faf57665bc
Ploye <musa7076@gmail.com>
2023-05-22 17:07:51 +0700

    CRUD

---
+++ alembic/versions/297ce70b63ce_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: 297ce70b63ce
+Revises: 67b53b3ecdc8
+Create Date: 2023-05-22 14:57:16.915306
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '297ce70b63ce'
+down_revision = '67b53b3ecdc8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import uuid
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, EmailStr, constr
 
 
@@ -29,3 +29,48 @@ class UserResponse(UserBaseSchema):
     id: uuid.UUID
     created_at: Optional[datetime]
     updated_at: Optional[datetime]
+
+
+class FilteredUserResponse(UserBaseSchema):
+    id: uuid.UUID
+
+
+class PostBaseSchema(BaseModel):
+    title: str
+    content: str
+    category: str
+    image: str
+    user_id: uuid.UUID | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class CreatePostSchema(PostBaseSchema):
+    pass
+
+
+class PostResponse(PostBaseSchema):
+    id: uuid.UUID
+    user: FilteredUserResponse
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+
+class UpdatePostSchema(BaseModel):
+    title: str
+    content: str
+    category: str
+    image: str
+    user_id: uuid.UUID | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class ListPostResponse(BaseModel):
+    status: str
+    results: int
+    posts: List[PostResponse]
